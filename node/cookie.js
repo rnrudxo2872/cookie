420b5141f28ac35fb806c303cb5d9da6db4e4ecf
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-02-24 22:55:53 +0900

    cookie module 설치 및 cookie 값 읽기

--- node/cookie.js
+++ node/cookie.js
@@ -1,8 +1,15 @@
 let http = require('http');
+let cookie = require('cookie')
 
-http.createServer(function(request,response){
-    // response.writeHead(200, {
-    //    'Set-Cookie': ['yummy_cookie=choco','tasty_cookie=strawberry']
-    // })
+http.createServer(function (request, response) {
+    console.log(request.headers.cookie)
+    let cookies = [];
+    if(request.headers.cookie !== undefined){
+    cookies = cookie.parse(request.headers.cookie);//parse는 undefined시 못찾아서 에러가 남.
+    }
+    console.log(cookies.yummy_cookie)
+    response.writeHead(200, {
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    })
     response.end('Cookie!');
 }).listen(3000);
