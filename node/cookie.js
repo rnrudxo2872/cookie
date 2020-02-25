6fbbcc563f168e4c361e11306e5364c826183ec4
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-02-25 23:56:22 +0900

    perment cookie

--- node/cookie.js
+++ node/cookie.js
@@ -9,7 +9,8 @@ http.createServer(function (request, response) {
     }
     console.log(cookies.yummy_cookie)
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry',
+    `Permanent=cookies; Max-Age=${60*60*24*30}`]
     })
     response.end('Cookie!');
 }).listen(3000);
