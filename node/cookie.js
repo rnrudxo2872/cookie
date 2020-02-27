1a86f715cccef2f553ae571f1c461bcd7fbc6a03
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-02-27 23:19:43 +0900

    topic : Secure

--- node/cookie.js
+++ node/cookie.js
@@ -4,13 +4,17 @@ let cookie = require('cookie')
 http.createServer(function (request, response) {
     console.log(request.headers.cookie)
     let cookies = [];
-    if(request.headers.cookie !== undefined){
-    cookies = cookie.parse(request.headers.cookie);//parse는 undefined시 못찾아서 에러가 남.
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie); //parse는 undefined시 못찾아서 에러가 남.
     }
     console.log(cookies.yummy_cookie)
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry',
-    `Permanent=cookies; Max-Age=${60*60*24*30}`]
+        'Set-Cookie': ['yummy_cookie=choco',
+        'tasty_cookie=strawberry; Secure',
+        `Permanent=cookies; Max-Age=${60*60*24*30}`,
+        'Secure=Secure; Secure',
+        'HttpOnly=HttpOnly; HttpOnly' //HTTP로서만 보임, javascript로 cookie값 추출 못함
+        ]
     })
     response.end('Cookie!');
 }).listen(3000);
