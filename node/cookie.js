8edf695c6b68b72dbc292fb75afdf21c36b59015
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-02-29 23:55:40 +0900

    cookie Path

--- node/cookie.js
+++ node/cookie.js
@@ -9,12 +9,15 @@ http.createServer(function (request, response) {
     }
     console.log(cookies.yummy_cookie)
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco',
+        'Set-Cookie': [
+        'yummy_cookie=choco',
         'tasty_cookie=strawberry; Secure',
         `Permanent=cookies; Max-Age=${60*60*24*30}`,
         'Secure=Secure; Secure',
-        'HttpOnly=HttpOnly; HttpOnly' //HTTP로서만 보임, javascript로 cookie값 추출 못함
-        ]
+        'HttpOnly=HttpOnly; HttpOnly', //HTTP로서만 보임, javascript로 cookie값 추출 못함
+        'Path=Path; Path=/cookie', //하위 디렉토리 cookie에서 cookie가 활성
+        'Domain=Domain; Domain=koo.net'
+    ]
     })
     response.end('Cookie!');
 }).listen(3000);
