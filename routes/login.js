a521bbff213ca08950139ada42292f447ab36fd0
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-03-10 23:29:23 +0900

    X

--- routes/login.js
+++ routes/login.js
@@ -4,13 +4,14 @@ var path = require('path');
 var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
-var authIsOwner = require('../lib/authLogin')
+var authIsOwner = require('../lib/authLogin');
+var cookie = require('cookie');
 
 
 router.get('', (req, res) => {
   title = `login`;
-  var list = template.list(req.list, ``);
-  var IsOwner = authIsOwner.IsOwner(req,res);
+  // var list = template.list(req.list, ``);
+  // var IsOwner = authIsOwner.IsOwner(req,res);
   console.log(IsOwner);
   var html = template.control_HTML(title, list, `<div id="article">
     <h2>${title}</h2>
